7b884b2b34bb5baa7a383ae2c51c2e08c4de8f51
Wiput Pootong <wiput.pootong@gmail.com>
2019-05-12 00:41:18 +0700

    :tada: initial commit

---
+++ two-sum.py
@@ -0,0 +1,14 @@
+"""
+LeetCode : Two Sum (Easy)
+Python 3
+"""
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for index, number in enumerate(nums):
+            expected = target - number
+            if expected in nums:
+                expected_index = nums.index(expected)
+                if expected_index == index:
+                    continue
+                return sorted([index, expected_index])
+        return []
