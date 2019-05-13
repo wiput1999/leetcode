2ebc5e6c63809397e5513e289a9631cccea1f3ee
Wiput Pootong <wiput.pootong@gmail.com>
2019-05-13 22:13:45 +0700

    :sparkles: add reverse integer

---
+++ reverse-integer.py
@@ -0,0 +1,20 @@
+"""
+LeetCode : Reverse Integer (Easy)
+Python 3
+"""
+class Solution:
+    def reverse(self, x: int) -> int:
+        # boolean if x < 0
+        minus = x < 0
+
+        # multiply -1 to negative
+        if minus:
+            x *= -1
+
+        x = int(str(x)[::-1])
+
+        # As problem spec result must in range otherwise return 0
+        if x not in range(-2 ** 31, 2 ** 31):
+            return 0
+
+        return -x if minus else x
